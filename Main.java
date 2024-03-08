1b6d75f859dc043633db74a3d21f6235c389e06c
Defita Syarif <syarifdefita@gmail.com>
2024-03-08 13:17:35 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("operasi penjumlahan 1225 % 82 =  \n" + (1225%82));
+        System.out.println(46.7*89);
+        String var1 = "Defita";
+        System.out.println("Hello, " + var1);
+        int var2 = 555;
+        System.out.print(var2 + var2);
+    }
+}
+/* 
+This is a comment. A code block that will not be executed. 
+*/
